d0f7c2dd59ead63bfa25c50072d15c1275ac898f
Aryesh404 <aryeshshivmare704@gmail.com>
2024-08-09 22:56:41 +0530

    Initial commit

---
+++ spring-ws-server/src/main/java/com/involveininnovation/chat/model/Message.java
@@ -0,0 +1,18 @@
+package com.involveininnovation.chat.model;
+
+import lombok.*;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+}
